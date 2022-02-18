811eecccc6285db5ef3c7fd8c5957dbb444a48e3
young-build <excaliwyver@outlook.com>
2022-02-18 13:14:28 +0800

    Modified file: -  imported navbar

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,10 +3,11 @@
   <title>Hackmark - references for html and css </title>
 </svelte:head>
 <script lang="ts">
+  import Navbar from "../components/navbar.svelte";
 </script>
-
+<Navbar/>
 <main>
   <h1>Welcome to hackmark</h1>
-  <p>This is a website that teaches you html, css, mark</p>
+  <p>This is a website that teaches you html, css and markdown</p>
 
 </main>
