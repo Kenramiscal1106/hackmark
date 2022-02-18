7c156b07166b2862e19746b4a7d3e37928db3d86
young-build <excaliwyver@outlook.com>
2022-02-18 13:11:32 +0800

    Added file - components

---
+++ src/components/navbar.svelte
@@ -0,0 +1,10 @@
+<nav class="navigation">
+  <a href="/home">home</a>
+  <a href="/">Index</a>
+  <a href="/html/introduction">HTML Introduction</a>
+</nav>
+<script lang="ts">
+</script>
+<style>
+  .navigation > a {display:block;}
+</style>
