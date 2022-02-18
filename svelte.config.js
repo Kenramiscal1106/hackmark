fd4bc93b7bb2e8b92a4ea3ff96222d0790c0b01a
young-build <excaliwyver@outlook.com>
2022-02-18 13:09:42 +0800

    Modified file: - reimported adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-// import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -6,10 +6,10 @@ const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
 	preprocess: preprocess(),
-/* 
+
 	kit: {
 		adapter: adapter()
-	} */
+	}
 };
 
 export default config;
