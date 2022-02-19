f46f872b82b7d1a39a5040c4b107bd56c58aa58c
young-build <excaliwyver@outlook.com>
2022-02-20 06:13:16 +0800

    Modified file: - added svelte:head

--- src/routes/html/introduction.svelte
+++ src/routes/html/introduction.svelte
@@ -2,6 +2,9 @@
   import Navbar from "../../components/navbar.svelte";
 </script>
 <Navbar/>
+<svelte:head>
+  <title>HTML Introduction</title>
+</svelte:head>
 <main>
   <h1>HTML Introduction</h1>
 </main>
