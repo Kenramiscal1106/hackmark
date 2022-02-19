2264e618ffba8a6d44dd8973f6332814809b6101
young-build <excaliwyver@outlook.com>
2022-02-20 06:12:58 +0800

    Modified file: - added a link

--- src/components/navbar.svelte
+++ src/components/navbar.svelte
@@ -2,6 +2,7 @@
   <a href="/home">home</a>
   <a href="/">Index</a>
   <a href="/html/introduction">HTML Introduction</a>
+  <a href="/css/introduction">CSS Introduction</a>
 </nav>
 <script lang="ts">
 </script>
