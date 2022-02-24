bcf354fee1ac260289b17ea94ded69f58bd8e16e
Kenramiscal1106 <ramiscalken@gmail.com>
2022-02-24 12:58:17 +0800

    Added file

---
+++ src/routes/markdown/home.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import Navbar from "../../components/navbar.svelte";
+</script>
+<Navbar/>
+<main>
+  <h1>Markdown</h1>
+  <p>Is a markup language used for documentations, messaging and READMEs.</p>
+</main>
