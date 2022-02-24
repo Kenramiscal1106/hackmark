63c95e66a579326a11c618986a05e7256cffe652
Kenramiscal1106 <ramiscalken@gmail.com>
2022-02-24 11:06:53 +0800

    Added file route svelte

---
+++ src/routes/css/home.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import Navbar from "../../components/navbar.svelte";
+</script>
+<svelte:head>
+  <title>CSS</title>
+</svelte:head>
+<Navbar/>
+<main>
+  <h1>Home</h1>
+</main>
