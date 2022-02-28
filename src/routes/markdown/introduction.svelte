1e2c1a810afedd20db46093e5c6ed91266747a8a
Kenramiscal1106 <ramiscalken@gmail.com>
2022-02-28 12:53:46 +0800

    Added file

---
+++ src/routes/markdown/introduction.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import Navbar from "../../components/navbar.svelte";
+</script>
+<svelte:head>
+  <title>Markdown introduction</title>
+</svelte:head>
+<Navbar extraLinks={[
+  {title:'Introduction',link:'/introduction'},
+  {title:'How To',link:'/how-to'},
+  {title:'Headings',link:'/headings'},
+  {title:'paragraphs and Styles',link:'/paragraph-and-styles'}
+]} firstPath="/markdown"/>
+<main>
+  <h1>Markdown</h1>
+  <p>Markdown has a declarative syntax</p>
+</main>
