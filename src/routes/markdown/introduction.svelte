8af3daa512da5b8e838af583aa854924768da404
Kenramiscal1106 <ramiscalken@gmail.com>
2022-02-28 14:13:59 +0800

    Modified file: - separated default component and the custom navbar component

--- src/routes/markdown/introduction.svelte
+++ src/routes/markdown/introduction.svelte
@@ -4,12 +4,13 @@
 <svelte:head>
   <title>Markdown introduction</title>
 </svelte:head>
-<Navbar extraLinks={[
+<Navbar vertical={false}/>
+<Navbar links={[
   {title:'Introduction',link:'/introduction'},
   {title:'How To',link:'/how-to'},
   {title:'Headings',link:'/headings'},
   {title:'paragraphs and Styles',link:'/paragraph-and-styles'}
-]} firstPath="/markdown"/>
+]} firstPath="/markdown" vertical={true}/>
 <main>
   <h1>Markdown</h1>
   <p>Markdown has a declarative syntax</p>
