657abb2499c7c8c63541795ffc491223d2b7e1bc
Kenramiscal1106 <ramiscalken@gmail.com>
2022-02-28 14:11:24 +0800

    Modified file: Changed the structure of the navbar

--- src/routes/markdown/index.svelte
+++ src/routes/markdown/index.svelte
@@ -4,12 +4,13 @@
 <svelte:head>
   <title>Markdown</title>
 </svelte:head>
-<Navbar extraLinks={[
+<Navbar vertical={false}/>
+<Navbar links={[
   {title:'Introduction',link:'/introduction'},
   {title:'How To',link:'/how-to'},
   {title:'Headings',link:'/headings'},
   {title:'paragraphs and Styles',link:'/paragraph-and-styles'}
-]} firstPath="/markdown" vertical={false}/>
+]} firstPath="/markdown" vertical={true}/>
 <main>
   <h1>Markdown</h1>
   <p>Is a markup language used for documentations, messaging and READMEs.</p>
