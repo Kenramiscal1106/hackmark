597af78386fe8503732f38630827608a20db3755
Kenramiscal1106 <ramiscalken@gmail.com>
2022-02-28 16:32:52 +0800

    Modified file: - added a vertical prop to the navbar to specify the target element - changed the order of the links in the linkFormat default value - removed div container - updated styling

--- src/components/navbar.svelte
+++ src/components/navbar.svelte
@@ -3,31 +3,40 @@
     link:string,
     title:string
   }
-  export let vertical:boolean = true;
+  export let target: string = '';
+  export let vertical:boolean = false;
   export let firstPath: string = '';
   export let links:linkFormat[] = [
+    {link:'/', title:'HOME'},
     {link:'/html',title:'HTML'},
     {link:'/css',title:'CSS'},
-    {link:"/markdown", title:'Markdown'},
-    {link:'/', title:'HOME'}
+    {link:"/markdown", title:'Markdown'}
   ]
 </script>
 <nav class={vertical ? 'sidebar' : 'vertical'}>
-  <div>
     {#each links as link}
-      <a href={`${firstPath}${link.link}`}>{link.title}</a>
+      <a href={`${firstPath}${link.link}`} class:active={target === link.link}>{link.title}</a>
     {/each}
-  </div>
  
 </nav>
 <style lang="scss" >
-  .sidebar a {
-  display: block;
+  @import '../../static/variables';
+  nav {
+  display: flex;
+  background: $nav_bg;
+  &.sidebar {
+    display: grid;
   }
-  .sidebar {
-    display: flex;
+  > a {
+    display: block;
+    color:$nav_text; 
+    padding: 0.75rem 1rem;
+    &:hover {
+      background: $hover_nav_bg;
+    }
+    &.active {
+      background: $nav_active;
+    }
   }
-  .vertical > div {
-    display: flex;
-  }
+}
 </style>
