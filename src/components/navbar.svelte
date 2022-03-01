aba647d41444c38b9ced214cb2f61e8fb1d4fe49
Kenramiscal1106 <ramiscalken@gmail.com>
2022-03-01 09:07:57 +0800

    Modified file: - removed fallback prop

--- src/components/navbar.svelte
+++ src/components/navbar.svelte
@@ -6,12 +6,7 @@
   export let target: string = '';
   export let vertical:boolean = false;
   export let firstPath: string = '';
-  export let links:linkFormat[] = [
-    {link:'/', title:'HOME'},
-    {link:'/html',title:'HTML'},
-    {link:'/css',title:'CSS'},
-    {link:"/markdown", title:'Markdown'}
-  ]
+  export let links:linkFormat[];
 </script>
 <nav class={vertical ? 'sidebar' : 'vertical'}>
     {#each links as link}
