5b3d49c5f3beb0d0554ee2b2f631e267819292d1
Kenramiscal1106 <ramiscalken@gmail.com>
2022-03-01 09:07:40 +0800

    Added file:

---
+++ src/routes/test/index.svelte
@@ -0,0 +1,17 @@
+<script>
+import Layout from "../../components/layout.svelte";
+import Navbar from "../../components/navbar.svelte";
+
+  
+</script>
+<Layout>
+  <Navbar slot="sidebar" links={[
+    {link:'/html', title: 'HTML'}, 
+    {link:'/css',title: 'CSS'},
+    {link:'/markdown',title:"markdown"}
+  ]} vertical={true}/>
+  <main slot="content">
+    user
+  </main>
+
+</Layout>
